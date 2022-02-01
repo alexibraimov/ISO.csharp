051cdd3dccde58e8baf0bc9e9b9dcfc6e17d994e
Alexander Ibraimov <59023460+AlexanderIbraimov@users.noreply.github.com>
2022-02-01 20:10:38 +0600

    added a language model

---
+++ src/ISO3166Lib/Helpers/IISOFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISO3166Lib.Helpers
+{
+    public interface IISOFacade
+    {
+        string Name { get; }
+
+    }
+}
