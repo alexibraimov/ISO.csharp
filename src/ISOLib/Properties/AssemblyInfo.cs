0f0790ab2d20e5543b5c2dd39999e844c24a007c
Alexander Ibraimov <59023460+AlexanderIbraimov@users.noreply.github.com>
2022-02-08 21:03:44 +0600

    refactoring

--- src/ISOLib/Properties/AssemblyInfo.cs
+++ src/ISOLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISO3166Lib")]
+[assembly: AssemblyTitle("ISOLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISO3166Lib")]
+[assembly: AssemblyProduct("ISOLib")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
